acc563612c2a9dbe450f89889da75cf99664de10
sattarmondol <sattarkha22@gmail.com>
2021-01-21 22:47:47 +0600

    second commit here

---
+++ assignment.js
@@ -0,0 +1,17 @@
+
+// first problem solving function.
+function kilometerToMeter(getKilometer) {
+    
+}
+// second problem solving function.
+function budgetCalculator() {
+    
+}
+// third problem solving function.
+function hotelCost() {
+    
+}
+// fourth problem solving function.
+function megaFriend() {
+    
+}
