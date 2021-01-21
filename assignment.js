bf7996e30ae1f51f361b9ea13a92d7b9e511b83a
sattarmondol <sattarkha22@gmail.com>
2021-01-22 01:05:29 +0600

    second problem solved

--- assignment.js
+++ assignment.js
@@ -3,15 +3,21 @@
 function kilometerToMeter(getKilometer) {
     if(getKilometer != 0 || getKilometer == 0){
         // 1 kilometer = 1000 meter. user input (getKilometer) parsing to float
-        var Meter = +getKilometer * 1000; 
+        var meter = +getKilometer * 1000; 
     }
-    return Meter;
+    return meter;
 }
 
 // second problem solving function.
-function budgetCalculator() {
-    
+function budgetCalculator(numberOfClock, numberOfPhone, numberOfLaptop) {
+
+    var clockPrice = numberOfClock * 50;
+    var phonePrice = numberOfPhone * 100;
+    var laptopPrice = numberOfLaptop * 500;
+    var totalPrice = clockPrice + phonePrice + laptopPrice;
+    return totalPrice;
 }
+
 // third problem solving function.
 function hotelCost() {
     
